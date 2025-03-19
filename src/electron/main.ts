036139c6f978cbb2f96201dfb034120b448bcc60
Michael Lamprecht <mlamprecht@student.tugraz.at>
2025-03-19 10:53:57 +0100

    init vite and electron

---
+++ src/electron/main.ts
@@ -0,0 +1,45 @@
+import { app, BrowserWindow } from 'electron';
+import { isDev } from './util.js';
+import { getPreloadPath, getUIPath } from './pathResolver.js';
+
+// Remove menu from application
+// Menu.setApplicationMenu(null);
+
+app.on('ready', () => {
+  const mainWindow = new BrowserWindow({
+    webPreferences: {
+      preload: getPreloadPath(),
+    },
+  });
+
+  if (isDev()) {
+    mainWindow.loadURL('http://localhost:5123');
+  } else {
+    mainWindow.loadFile(getUIPath());
+  }
+
+  handleCloseEvents(mainWindow);
+});
+
+function handleCloseEvents(mainWindow: BrowserWindow) {
+  let willClose = false;
+
+  mainWindow.on('close', (e) => {
+    if (willClose) {
+      return;
+    }
+    e.preventDefault();
+    mainWindow.hide();
+    if (app.dock) {
+      app.dock.hide();
+    }
+  });
+
+  app.on('before-quit', () => {
+    willClose = true;
+  });
+
+  mainWindow.on('show', () => {
+    willClose = false;
+  });
+}
